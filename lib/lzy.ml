8a232abfe1876bc49a362f818cd25dd87d8e08d5
Jeremy Yallop <yallop@gmail.com>
2018-04-18 17:31:16 +0100

    There's a simpler covariant 'lazy' implementation without subtypes.

--- lib/lzy.ml
+++ lib/lzy.ml
@@ -5,15 +5,16 @@
    extra polymorphism in 'let'.
 *)
 
-   
-
-module Lzy (S: sig type (-_,+_) sub val refl : ('a,'a) sub val coerce : ('a,'b) sub -> 'a -> 'b end) :
+module type LAZY =
 sig
   type +_ t
 
-  val lzy : (unit -> 'a) -> 'a t
-  val fce : 'a t -> 'a
-end =
+  val delay : (unit -> 'a) -> 'a t
+  val force : 'a t -> 'a
+end
+
+
+module Lazy_sub (S: sig type (-_,+_) sub val refl : ('a,'a) sub val coerce : ('a,'b) sub -> 'a -> 'b end) : LAZY =
 struct
   open S
 
@@ -24,13 +25,34 @@ struct
 
   type +_ t = L : ('a, 'b) sub * 'a lazy_cell ref -> 'b t
 
-  let lzy f = L (refl, ref (Thunk f))
+  let delay f = L (refl, ref (Thunk f))
 
-  let fce (L (sub, r)) =
+  let force (L (sub, r)) =
     match !r with
     | Thunk f -> (match f () with
                   | v -> r := Value v; coerce sub v
                   | exception e -> r := Exception e; raise e)
     | Value v -> coerce sub v
     | Exception e -> raise e
+end
+
+module Lazy_thunk : LAZY =
+struct
+  type +'a lazy_cell =
+    | Thunk of (unit -> 'a)
+    | Value of 'a
+    | Exception of exn
+
+  type +'a t = (unit -> 'a)
+
+  let delay f =
+    let r = ref (Thunk f) in 
+    fun () -> match !r with
+      | Thunk f -> (match f () with
+          | v -> r := Value v; v
+          | exception e -> r := Exception e; raise e)
+      | Value v -> v
+      | Exception e -> raise e
+
+  let force f = f ()
 end
